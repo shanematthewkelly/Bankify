f1d3ed774ae35a059eecf62e12a1a2b01ed9a1bd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 00:39:54 +0000

    Status Bar Set To Transparent - Full Screen Application

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.white, // navigation bar color
+      statusBarIconBrightness: Brightness.dark, // status bar icons' color
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(
       title: 'Bankify',
       theme: ThemeData(
         //Application Global Theme
         primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Preloader(),
       debugShowCheckedModeBanner: false,
